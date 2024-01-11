1c8619544bd57a4c7bc5b0a395e16f8deea58e03
JLisarte <juliano.lisarte@gmail.com>
2024-01-11 18:09:09 -0300

    Add routes and middleware to server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express")
 
+const routes = require("./routes")
+
 const app = express()
 
-app.get("/", (request, response) => {
-  
-})
+app.use(express.json())
+
+app.use(routes)
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
